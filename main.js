951255da0a7a0d10ba1f6abde980430890f5284a
Jaqueline <jaqueline.torres.perez.unam@gmail.com>
2024-07-18 19:04:44 -0600

    update: se agregó color al fondo, se cambio la ruta de css

--- main.js
+++ main.js
@@ -15,15 +15,13 @@ const guessNumber = () => {
         <h2>¿Es este tu número: ${guess}?</h2>
         <button id="yes-button" class="btn btn-success m-2">Sí</button>
         <button id="no-button" class="btn btn-danger m-2">No</button>
-        <button id="reset-game" class="btn btn-warning mt-3">Reiniciar Juego</button>
       </div>
     `;
 
     document.getElementById('yes-button').addEventListener('click', () => {
       document.getElementById('game-container').innerHTML = `
         <h2 class="text-center mt-3">Tu número es el ${guess}, excelente decisión</h2>
-        <button id="start-game" class="btn btn-info m-2">Reset Game</button>
-        
+        <button id="reset-game" class="btn btn-warning mt-3">Reiniciar Juego</button>
       `;
       document.getElementById('reset-game').addEventListener('click', resetGame);
     });
@@ -34,7 +32,6 @@ const guessNumber = () => {
           <h2>¿Tu número es mayor que ${guess}?</h2>
           <button id="higher-button" class="btn btn-primary m-2">Sí</button>
           <button id="lower-button" class="btn btn-secondary m-2">No</button>
-          <button id="start-game" class="btn btn-info m-2">Reset Game</button>
         </div>
       `;
 
@@ -52,22 +49,20 @@ const guessNumber = () => {
 };
 
 const resetGame = () => {
-  document.getElementById('game-container').innerHTML = `<h2 class="text-center mt-3">Juego reiniciado. Haz clic en "Iniciar Juego" para empezar.</h2>`;
+  document.getElementById('game-container').innerHTML = `
+    <div class="text-center mt-5">
+      <h2>Haz clic en "Iniciar Juego" para empezar.</h2>
+      <button id="start-game" class="btn btn-primary mt-3">Iniciar Juego</button>
+    </div>
+  `;
+
+  document.getElementById('start-game').addEventListener('click', startGame);
 };
 
-document.getElementById("start-game").addEventListener('click', () => {
+const startGame = () => {
   left = 0;
   right = numbers.length - 1;
   guessNumber();
-});
+};
 
-document.getElementById("reset-game").addEventListener('click', resetGame);
-
-document.body.innerHTML = `
-  <div class="container text-center mt-5">
-    <div id="game-container">
-      <h2>Haz clic en "Iniciar Juego" para empezar.</h2>
-    </div>
-    <button id="start-game" class="btn btn-primary mt-3">Iniciar Juego</button>
-  </div>
-`;
+document.getElementById('start-game').addEventListener('click', startGame);
